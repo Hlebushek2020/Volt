58fea2d1a1666806bf979a56a341d7608dc2b20b
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2022-12-10 22:58:35 +0300

    Update BotPingService.cs

--- VoltBot/Services/BotPingService.cs
+++ VoltBot/Services/BotPingService.cs
@@ -1,20 +1,29 @@
 ﻿using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
+using VoltBot.Logs.Providers;
+using VoltBot.Logs;
 
 namespace VoltBot.Services
 {
     internal class BotPingService
     {
+        private readonly ILogger _defaultLogger = null;// LoggerFactory.Current.CreateLogger<DefaultLoggerProvider>();
+
         public async Task Ping(DiscordClient sender, MessageCreateEventArgs e)
         {
+            EventId eventId = new EventId(0, "Ping");
+
             string messageContent = e.Message.Content.Trim();
 
             if ($"<@{sender.CurrentUser.Id}>".Equals(messageContent) ||
                 messageContent.Equals(Settings.Settings.Current.BotPrefix, StringComparison.InvariantCultureIgnoreCase))
             {
+                _defaultLogger.LogInformation(eventId, $"{e.Guild.Name}, {e.Channel.Name}, {e.Message.Id}");
+
                 DiscordMember discordMember = await e.Message.Channel.Guild.GetMemberAsync(e.Message.Author.Id);
                 await e.Message.RespondAsync($"**Виляет хвостиком и смотрит на {discordMember.DisplayName} в ожидании команды**");
             }
