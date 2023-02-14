e26bf4cf3286c8631a3146d0b9ce9a359b577baa
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2023-02-14 20:41:49 +0300

    Update AdministratorCommandModule.cs

--- VoltBot/Commands/AdministratorCommandModule.cs
+++ VoltBot/Commands/AdministratorCommandModule.cs
@@ -57,55 +57,66 @@ namespace VoltBot.Commands
                 EventId eventId = new EventId(0, $"Command: {ctx.Command.Name}");
                 DiscordMessage discordMessage = ctx.Message;
 
-                DiscordEmbedBuilder reportEmbed = new DiscordEmbedBuilder()
-                    .WithColor(Constants.SuccessColor)
-                    .WithTitle("Bug-Report")
-                    .AddField("Author", ctx.User.Username + "#" + ctx.User.Discriminator)
-                    .AddField("Guild", ctx.Guild.Name)
-                    .AddField("Date", discordMessage.CreationTimestamp.LocalDateTime.ToString("dd.MM.yyyy HH:mm:ss"));
-
-                if (!string.IsNullOrEmpty(description))
-                {
-                    reportEmbed.AddField("Description", description);
-                }
-
-                DiscordMessageBuilder reportMessage = new DiscordMessageBuilder().WithEmbed(reportEmbed);
-                foreach (DiscordAttachment attachment in discordMessage.Attachments)
-                {
-                    try
-                    {
-                        using HttpClient client = new HttpClient();
-                        Stream fileStream = await client.GetStreamAsync(attachment.Url);
-                        string fileName = attachment.FileName ?? Guid.NewGuid().ToString();
-                        reportMessage.AddFile(fileName, fileStream);
-                    }
-                    catch (Exception ex)
-                    {
-                        _defaultLogger.LogWarning(eventId, ex, "");
-                    }
-                }
-
                 DiscordMessage referencedMessage = discordMessage.ReferencedMessage;
 
-                if (referencedMessage != null)
+                if (referencedMessage == null && discordMessage.Attachments.Count == 0 &&
+                    string.IsNullOrEmpty(description))
                 {
-                    if (!string.IsNullOrEmpty(referencedMessage.Content))
-                    {
-                        reportEmbed.AddField("Reference Message", referencedMessage.Content);
-                    }
-
-                    if (referencedMessage.Embeds != null)
-                    {
-                        reportMessage.AddEmbeds(referencedMessage.Embeds);
-                    }
+                    discordEmbed.WithColor(Constants.ErrorColor)
+                        .WithDescription(
+                            "Пустой баг-репорт! Баг-репорт должен содержать описание и/или вложения и/или быть ответом на другое сообщение!");
                 }
+                else
+                {
+                    DiscordEmbedBuilder reportEmbed = new DiscordEmbedBuilder()
+                        .WithColor(Constants.SuccessColor)
+                        .WithTitle("Bug-Report")
+                        .AddField("Author", ctx.User.Username + "#" + ctx.User.Discriminator)
+                        .AddField("Guild", ctx.Guild.Name)
+                        .AddField("Date",
+                            discordMessage.CreationTimestamp.LocalDateTime.ToString("dd.MM.yyyy HH:mm:ss"));
 
-                DiscordGuild reportGuild = await ctx.Client.GetGuildAsync(_settings.BugReportServer);
-                DiscordChannel reportChannel = reportGuild.GetChannel(_settings.BugReportChannel);
+                    if (!string.IsNullOrEmpty(description))
+                    {
+                        reportEmbed.AddField("Description", description);
+                    }
 
-                await reportChannel.SendMessageAsync(reportMessage);
+                    DiscordMessageBuilder reportMessage = new DiscordMessageBuilder().WithEmbed(reportEmbed);
+                    foreach (DiscordAttachment attachment in discordMessage.Attachments)
+                    {
+                        try
+                        {
+                            using HttpClient client = new HttpClient();
+                            Stream fileStream = await client.GetStreamAsync(attachment.Url);
+                            string fileName = attachment.FileName ?? Guid.NewGuid().ToString();
+                            reportMessage.AddFile(fileName, fileStream);
+                        }
+                        catch (Exception ex)
+                        {
+                            _defaultLogger.LogWarning(eventId, ex, "");
+                        }
+                    }
 
-                discordEmbed.WithDescription("Баг-репорт успешно отправлен!");
+                    if (referencedMessage != null)
+                    {
+                        if (!string.IsNullOrEmpty(referencedMessage.Content))
+                        {
+                            reportEmbed.AddField("Reference Message", referencedMessage.Content);
+                        }
+
+                        if (referencedMessage.Embeds != null)
+                        {
+                            reportMessage.AddEmbeds(referencedMessage.Embeds);
+                        }
+                    }
+
+                    DiscordGuild reportGuild = await ctx.Client.GetGuildAsync(_settings.BugReportServer);
+                    DiscordChannel reportChannel = reportGuild.GetChannel(_settings.BugReportChannel);
+
+                    await reportChannel.SendMessageAsync(reportMessage);
+
+                    discordEmbed.WithDescription("Баг-репорт успешно отправлен!");
+                }
             }
             else
             {
